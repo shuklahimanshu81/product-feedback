bed85dfc20c4de79df8ee1abd10aa9beb7c35471
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-08-30 21:26:40 +0530

    feat : Tailwind Setup and Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SuggestionsPage from "./Pages/SuggestionPage";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SuggestionsPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Pages/SuggestionPage.js
@@ -0,0 +1,5 @@
+const SuggestionsPage = () => {
+  return <div className="bg-red-500 p-10 m-0">Himanshu</div>;
+};
+
+export default SuggestionsPage;
